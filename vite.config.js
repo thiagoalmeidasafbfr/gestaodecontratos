2b0f0f1c960db1bdaf9cb8575ce5a9b264a5a8b0
Thiago Almeida <thiago.almeida@safbfr.com.br>
2025-11-12 11:45:15 -0300

    Teste

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // O 'base' é crucial para o GitHub Pages
+  // Substitua 'saf-analytics-app' pelo nome exato do SEU repositório no GitHub
+  // Se o nome do repositório for diferente, mude aqui.
+  base: '/gestaodecontratos/', 
   plugins: [react()],
 })
